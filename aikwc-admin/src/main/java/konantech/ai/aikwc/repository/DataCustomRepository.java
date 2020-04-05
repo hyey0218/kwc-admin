015a7696fa4dc31e360a3fa53fbb34ff8e9e9bdb
hyey0218 <hyey0218@gmail.com>
2020-04-05 22:51:04 +0900

    이거다

--- aikwc-admin/src/main/java/konantech/ai/aikwc/repository/DataCustomRepository.java
+++ aikwc-admin/src/main/java/konantech/ai/aikwc/repository/DataCustomRepository.java
@@ -2,11 +2,26 @@ package konantech.ai.aikwc.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import konantech.ai.aikwc.entity.KTask;
 
 @Repository
 public class DataCustomRepository {
 	@PersistenceContext
-	EntityManager entityManager;
+	EntityManager em;
+	
+	@Transactional
+	public void saveTest(String table) {
+//		Query query = em.createNativeQuery("select * from "+table,KTask.class);
+//		KTask k = (KTask) query.getSingleResult();
+//		System.out.println(k.getPk());
+		
+		Query query = em.createNativeQuery("insert into " + table + "( pk )" + "values ( 33 )");
+		query.executeUpdate();
+		
+	}
 }
